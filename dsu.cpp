611bdd6b46204f6a3412699708d9f5d9aae01115
Mohamedboukerche22 <mohamedboukerche55@gmail.com>
2025-05-16 18:49:54 +0100

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,50 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class DSU {
-private:
-    vector<int> parent, size;
+const int N = 1e5 + 5; 
 
-public:
-    DSU(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
+int parent[N];  int rank_[N];  
+
+void make_set(int n) {
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+        rank_[i] = 0;
     }
-    int find(int u) {
-        if (parent[u] != u) {
-            parent[u] = find(parent[u]);  
-        }
-        return parent[u];
-    }
-    void union_sets(int u, int v) {
-        int root_u = find(u);
-        int root_v = find(v);
-        
-        if (root_u != root_v) {
-            if (size[root_u] < size[root_v]) {
-                swap(root_u, root_v);
-            }
+}
+
+int find(int u) {
+    if (u != parent[u])
+        parent[u] = find(parent[u]);
+    return parent[u];
+}
+
+void union_sets(int u, int v) {
+    int root_u = find(u);
+    int root_v = find(v);
+    if (root_u != root_v) {
+        // Union by rank
+        if (rank_[root_u] < rank_[root_v]) {
+            parent[root_u] = root_v;
+        } else if (rank_[root_u] > rank_[root_v]) {
             parent[root_v] = root_u;
-            size[root_u] += size[root_v];
+        } else {
+            parent[root_v] = root_u;
+            rank_[root_u]++;
         }
     }
-    bool connected(int u, int v) {
-        return find(u) == find(v);
-    }
-};
+}
+
 
 int main() {
-    int n = 10; 
-    DSU dsu(n);
+    int n ; 
+    cin >>n;
+    make_set(n);
 
-    dsu.union_sets(1, 2);
-    dsu.union_sets(2, 3);
-    dsu.union_sets(4, 5);
+  /*  union_sets(1, 2);
+    union_sets(3, 4);
+    union_sets(2, 3);*/
+    
 
-    cout << "1 and 3 are connected: " << (dsu.connected(1, 3) ? "Yes" : "No") << endl;
-    cout << "1 and 4 are connected: " << (dsu.connected(1, 4) ? "Yes" : "No") << endl;
+    // Check if 1 and 4 are connected
+    if (find(1) == find(4))
+        cout << "1 and 4 are in the same set\n";
+    else
+        cout << "1 and 4 are in different sets\n";
 
     return 0;
 }
