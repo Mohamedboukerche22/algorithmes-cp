7d443c53c5b82a27f812c31cc116daadd56f99f4
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2025-12-25 20:05:41 +0100

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,44 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define fastAOI ios::sync_with_stdio(false); cin.tie(nullptr);
 #define ll long long
 #define all(x) (x).begin(), (x).end()
 #define pb push_back
 #define sz(x) (int)(x).size()
+const int N = 2e5 +1;
+ 
+// opitimized version 
+// Path Compression
+// Union by Size
 
-int parent[100005];
-int find(int x){
-    if(parent[x] == x){
-        return x;
-    }
-    return parent[x] = find(parent[x]);
+int parent[N], SZ[N];
+
+void make_set(int v){
+    parent[v] = v;
+    SZ[v] = 1;
 }
-void unions(int a,int b){
+
+int find(int v){
+    if(v != parent[v]){
+        parent[v] = find(parent[v]);
+    }
+    return parent[v];
+}
+
+void Union(int a, int b){
     a = find(a);
     b = find(b);
-    if(a != b)parent[b] = a;
+    if(a != b){
+        if(SZ[a] < SZ[b]){
+            swap(a,b); 
+        }
+        parent[b] = a;
+        SZ[a] += SZ[b];
+    }
+
+
 }
+
+void solve(){
+       int n,m;
+       cin >> n>>m;
+       vector<vector<int>>adj(m+1);
+       for(int i = 0 ; i< n ;i++){
+        int u,v;
+        cin >>u >>v;
+        adj[u].pb(v);
+        adj[u].pb(v);
+       }
+       int a,b;
+       cin >>a >>b;
+       Union(a,b);
+       // do something
+      
+ 
+}
+ 
 int main() {
     fastAOI;
     int t = 1;
-    // cin >> t;
-    while (t--) {
-       int n,m;
-       cin >> n>>m;
-       for(int i = 1 ; i<n+1;i++){
-        parent[i] = i;
-       }
-       for(int i = 0 ; i< m ;i++){
-        int a,b;
-        cin >>a>>b;
-        unions(a,b);
-       }
-       int count = 0;
-       for(int i = 1;i<=n;i++){
-        if(find(i) == i)count++;
-       }
-       cout<<count;
+    while (t--)
+    {
+        solve();
     }
     return 0;
 }
